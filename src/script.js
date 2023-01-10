e804e094c5982a4bad792d301b8cf4cba00ab75b
toukoms <tokiniaina090605@gmail.com>
2023-01-10 21:13:05 +0300

    fix conflits

--- src/script.js
+++ src/script.js
@@ -1,12 +1,39 @@
+const urlRandom = "https://api.chucknorris.io/jokes/random";
+const changeBackground = document.querySelector("#change-background");
+const randomButton = document.querySelector("#random-btn");
+const quotes = document.querySelector("#quotes-text");
 const categorySelect = document.getElementById("categories");
 let allCategories = [];
 
-// Get all categories available from API
-fetchCategories().then((categories) => {
-    allCategories = categories
+categorySelect.addEventListener("change", () => {
+  // Get all categories available from API
+  fetchCategories().then((categories) => {
+    allCategories = categories;
     // filter categories and display it to the screen
     displayCategories();
-})
+  });
+});
+
+changeBackground.addEventListener("click", () => {
+  changeBackground.classList.toggle("nightlight");
+  if (changeBackground.classList.contains("nightlight")) {
+    changeBackground.textContent = "sunny";
+    document.body.style.backgroundImage =
+      "url('images/night-desert-background.png')";
+  } else {
+    changeBackground.textContent = "nightlight";
+    document.body.style.backgroundImage = "url('images/desert-background.jpg')";
+  }
+});
+
+randomButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  apiCall(urlRandom);
+});
+
+(function onLoad() {
+  apiCall(urlRandom);
+})();
 
 
 // ======================= FUNCTIONS ======================== //
@@ -14,36 +41,59 @@ fetchCategories().then((categories) => {
 /**
  * Show an error message in the console
  */
-function handleError(error) {
-    console.error(error);
+function handleErrors(error) {
+  quotBox.innerHTML = `
+  Oops! Something went wrong. Please reload the page and try again.`;
+  console.error(error.message);
 }
 
 /**
  * Fetch all available categories
  */
 async function fetchCategories() {
-    try {
-        let res = await fetch("https://api.chucknorris.io/jokes/categories");
-        return await res.json();
-    } catch (err) {
-        handleError(err);
-    }
+  try {
+    let res = await fetch("https://api.chucknorris.io/jokes/categories");
+    return await res.json();
+  } catch (err) {
+    handleErrors(err);
+  }
 }
 
-/** 
+/**
  * Add options to the category select element with all available categories,
- * excluding "explicit", "political" and "religion". 
+ * excluding "explicit", "political" and "religion".
  */
 function displayCategories() {
-    console.log(allCategories);
-    for (let i = 0; i < allCategories.length; i++) {
-        let category = allCategories[i];
-        if (["explicit", "political", "religion"].includes(category)) {
-            continue;
-        }
-        let option = document.createElement("option");
-        option.value = category;
-        option.textContent = category;
-        categorySelect.appendChild(option);
+  console.log(allCategories);
+  for (let i = 0; i < allCategories.length; i++) {
+    let category = allCategories[i];
+    if (["explicit", "political", "religion"].includes(category)) {
+      continue;
     }
+    let option = document.createElement("option");
+    option.value = category;
+    option.textContent = category;
+    categorySelect.appendChild(option);
+  }
+}
+
+function showQuote(result, url) {
+  console.log(url);
+  console.log(result.value);
+  quotes.textContent = result.value;
+}
+
+function apiCall(url) {
+  const getData = async (url) => {
+    console.log(url);
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  };
+
+  getData(url)
+    .then((data) => {
+      showQuote(data, url);
+    })
+    .catch((error) => handleErrors(error));
 }
