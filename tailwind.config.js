e3289eee233fb780a29851ecb2cef98828a8d36c
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-01-24 19:45:50 +0200

    gitignore

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./src/**/*.js",
+    './index.html',
+],
+
   theme: {
     container: {
       center: true,
