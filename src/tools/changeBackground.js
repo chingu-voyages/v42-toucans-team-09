b6bf779cabd57757764550c11f833827dcc724ca
Yana Zahoruiko <yana.zagoruiko2@gmail.com>
2023-01-25 14:50:44 +0200

    add alt to icon

--- src/tools/changeBackground.js
+++ src/tools/changeBackground.js
@@ -9,14 +9,15 @@ const changeBackground = document.querySelector("#change-background");
 changeBackground.addEventListener("click", changeBackgroundByClick);
 
 function changeBackgroundByClick() {
-  console.log(1);
   const html=document.documentElement;
   html.classList.toggle("dark");
   if (html.classList.contains("dark")){
     changeBackground.src="img/dark-bg.png"
+    changeBackground.alt="Switch to light background color"
   }
   else {
     changeBackground.src="img/light-bg.png"
+    changeBackground.alt="Switch to dark background color"
   }
 }
 
