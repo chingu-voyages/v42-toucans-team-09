334825fdf7e5ddfed2f00c5683579af016387b8e
Andrei-Bogdan Andrici <andriciandrei15@yahoo.com>
2023-01-26 14:29:03 +0200

    add quotes page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    "./src/**/*.js",
-    './index.html',
-],
+  darkMode: "class",
+  // modified content path to "./*.html" so all files
+  // ending in .html will be considered
+  content: ["./src/**/*.js", "./*.html"],
 
   theme: {
     // container: {
     //   center: true,
     // },
     screens: {
-      sm: '580px',
-      md: '768px',
-      lg: '976px',
-      xl:'1280px',
-      '2xl': '1440px',
+      sm: "580px",
+      md: "768px",
+      lg: "976px",
+      xl: "1280px",
+      "2xl": "1440px",
     },
 
-    fontFamily:{
-      sans: ['Roboto','sans-serif']
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
     },
     extend: {
       boxShadow: {
-        'btn': '0px 1.4433px 10.8247px 0.72165px rgba(0, 0, 0, 0.25)',
+        btn: "0px 1.4433px 10.8247px 0.72165px rgba(0, 0, 0, 0.25)",
       },
       width: {
-        '407': '25rem',
-        '679':'42.4rem',
+        407: "25rem",
+        679: "42.4rem",
       },
-      height:{
-        '181': '11.3rem'
-      }
+      height: {
+        181: "11.3rem",
+      },
     },
   },
   plugins: [],
-}
+};
